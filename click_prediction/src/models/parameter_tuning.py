47972751686a749743a5dcdeb2c9dea79d898069
abhijit <abgairola@gmail.com>
2023-03-24 20:41:56 +0530

    removed requirements.txt

--- click_prediction/src/models/parameter_tuning.py
+++ click_prediction/src/models/parameter_tuning.py
@@ -12,7 +12,67 @@ from config import config
 
 import xgboost as xgb
 import lightgbm as lgb
+from sklearn.ensemble import RandomForestClassifier
 
+# def objective(
+#     trial: optuna.Trial,
+#     X_train: pd.DataFrame,
+#     X_valid: pd.DataFrame,
+#     y_train: pd.DataFrame,
+#     y_valid: pd.DataFrame,
+#     smote: bool,
+# ):
+#     """The objective function that returns the f1-score for a model fitted on parameters drawn from specified distribution."""
+
+#     target_encoder = TargetEncoder()
+#     categorical_encoder = preprocessors.CategoricalTransformer(config.FEATURES)
+#     categorical_mapper = preprocessors.CategoricalMapper(config.FEATURES)
+#     if smote:
+#         smote_nc = SMOTEN(random_state=config.SEED)
+
+#     max_depth = trial.suggest_int(name="max_depth", low=3, high=7)
+#     learning_rate = trial.suggest_float(name="learning_rate", low=0.001, high=0.1)
+#     n_estimators = trial.suggest_int(name="n_estimators", low=50, high=100)
+#     subsample = trial.suggest_loguniform("subsample", 0.01, 1.0)
+#     gamma = trial.suggest_loguniform(name="gamma", low=1e-8, high=1)
+#     colsample_bytree = trial.suggest_loguniform("colsample_bytree", 0.01, 1.0)
+
+#     weight = len(y_train[y_train == 0]) / len(y_train[y_train == 1])
+#     params = {
+#         "max_depth": max_depth,
+#         "learning_rate": learning_rate,
+#         "n_estimators": n_estimators,
+#         "subsample": subsample,
+#         "colsample_bytree": colsample_bytree,
+#         "gamma": gamma,
+#         "enable_categorical": True,
+#         "tree_method": "hist",
+#         "scale_pos_weight": weight,
+#     }
+#     classifier = xgb.XGBClassifier(**params)
+
+#     if smote:
+#         pipeline = Pipeline(
+#             [
+#                 ("categorical_encoder", categorical_encoder),
+#                 ("categorical_mapper", categorical_mapper),
+#                 ("SMOTENC", smote_nc),
+#                 ("target_encoder", target_encoder),
+#                 ("clf", classifier),
+#             ]
+#         )
+#     else:
+#         pipeline = Pipeline(
+#             [
+#                 ("categorical_encoder", categorical_encoder),
+#                 ("categorical_mapper", categorical_mapper),
+#                 ("target_encoder", target_encoder),
+#                 ("clf", classifier),
+#             ]
+#         )
+#     classifier = pipeline.fit(X=X_train, y=y_train)
+#     y_pred = pipeline.predict(X=X_valid)
+#     return f1_score(y_true=y_valid, y_pred=y_pred)
 
 def objective(
     trial: optuna.Trial,
@@ -30,6 +90,12 @@ def objective(
     if smote:
         smote_nc = SMOTEN(random_state=config.SEED)
 
+    model_dict = {
+        "xgb": xgb.XGBClassifier,
+        "rf": RandomForestClassifier,
+    }
+    model_type = trial.suggest_categorical(name="model_type", choices=["xgb", "rf"])
+    sample = trial.suggest_float(name="sample", low=0.1, high=0.3)
     max_depth = trial.suggest_int(name="max_depth", low=3, high=7)
     learning_rate = trial.suggest_float(name="learning_rate", low=0.001, high=0.1)
     n_estimators = trial.suggest_int(name="n_estimators", low=50, high=100)
@@ -37,19 +103,28 @@ def objective(
     gamma = trial.suggest_loguniform(name="gamma", low=1e-8, high=1)
     colsample_bytree = trial.suggest_loguniform("colsample_bytree", 0.01, 1.0)
 
+    # X_train, y_train = undersample(X_train, y_train, sample=0.2)
     weight = len(y_train[y_train == 0]) / len(y_train[y_train == 1])
     params = {
         "max_depth": max_depth,
-        "learning_rate": learning_rate,
         "n_estimators": n_estimators,
-        "subsample": subsample,
-        "colsample_bytree": colsample_bytree,
-        "gamma": gamma,
-        "enable_categorical": True,
-        "tree_method": "hist",
-        "scale_pos_weight": weight,
     }
-    classifier = xgb.XGBClassifier(**params)
+    if smote:
+        pass
+    else:
+        if model_type == "xgb":
+            params["scale_pos_weight"] = weight
+        else:
+            params["class_weight"] = "balanced"
+    if model_type == "xgb":
+        params["learning_rate"] = learning_rate
+        params["gamma"] = gamma
+        params["enable_categorical"] = True
+        params["tree_method"] = "hist"
+        params["subsample"] = subsample
+        params["scale_pos_weight"] = weight
+        params["colsample_bytree"] = colsample_bytree
+    classifier = model_dict[model_type](**params)
 
     if smote:
         pipeline = Pipeline(
@@ -72,7 +147,9 @@ def objective(
         )
     classifier = pipeline.fit(X=X_train, y=y_train)
     y_pred = pipeline.predict(X=X_valid)
+    y_pred_proba = pipeline.predict_proba(X_valid)
     return f1_score(y_true=y_valid, y_pred=y_pred)
+
 
 
 def tune_hyperparams(X_train, X_valid, y_train, y_valid, smote=False) -> optuna.study:
--- click_prediction/src/utils.py
+++ click_prediction/src/utils.py
@@ -1,10 +1,10 @@
 from sklearn.model_selection import StratifiedKFold
 from config import config
-from sklearn.feature_selection import chi2
+from scipy.stats import chi2_contingency
 import pandas as pd
+from typing import List
 
-
-def create_folds(df, y):
+def create_folds(df: pd.DataFrame, y: pd.Series) -> pd.DataFrame:
     """
     helper function to create folds using StratifiedKFold validation
     """
@@ -18,16 +18,28 @@ def create_folds(df, y):
     return df
 
 
-def feature_selection_chisq(df, target, cols=None):
-    """
-    chi-sq function for feature selection
-    """
-    df = df.copy()
 
-    scores = chi2(df, target)
-    res = (
-        pd.Series(scores[1], index=df.columns, name="p_values")
-        .sort_values(ascending=False)
-        .reset_index()
-    )
-    return res
+
+def get_chisq_result(df: pd.DataFrame, target: str, skip:List[str]) -> pd.DataFrame:
+    '''
+    Calculates the p-value of the chi2 test of association between target column and the df. 
+    
+    Returns a dataframe with the column names, and the associated p value 
+    '''
+    column1 = []
+    column2 = []
+    chisq_p_value = []
+
+    colnames = df.columns.tolist()
+    for idx1 in range(len(colnames)- 1):
+        col1 = colnames[idx1]
+        col2 = config.TARGET
+
+        if col1 not in skip:
+            p_value = chi2_contingency(
+                pd.crosstab(index=df[col1], columns=df[col2]))[1]
+            column1.append(col1)
+            column2.append(col2)
+            chisq_p_value.append(p_value)
+    result = pd.DataFrame({"col1": column1, "col2": column2, "p_value": p_value})
+    return result
